631937bb67ba6c05db8837bf721e0ed2fb298548
shayan009 <shayan.deba0930@gmail.com>
2021-06-22 22:09:40 +0530

    added discovery server

--- src/main/java/com/shayan/book_management/ConfigServerApp.java
+++ src/main/java/com/shayan/book_management/ConfigServerApp.java
@@ -3,10 +3,12 @@ package com.shayan.book_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class ConfigServerApp {
 
 	
