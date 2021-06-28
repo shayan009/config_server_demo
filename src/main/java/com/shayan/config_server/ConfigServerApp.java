45c2c1e880b2b5f0d28cd7b1407396dc65f2e343
shayan009 <shayan.deba0930@gmail.com>
2021-06-29 01:01:16 +0530

    some code changes

--- src/main/java/com/shayan/config_server/ConfigServerApp.java
+++ src/main/java/com/shayan/config_server/ConfigServerApp.java
@@ -15,33 +15,13 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApp extends MvcConfiguration {
+public class ConfigServerApp  {
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApp.class, args);
 	}
-	 @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	         return application.sources(ConfigServerApp.class);
-	    }
+	
 	 
-	@Bean
-	public TomcatServletWebServerFactory servletContainerFactory() {
-	    return new TomcatServletWebServerFactory() {
-	    
-	      @Override
-	      protected TomcatWebServer getTomcatWebServer(Tomcat tomcat) {
-	          // webapps directory does not exist by default, needs to be created
-	          new File(tomcat.getServer().getCatalinaBase(), "webapps").mkdirs();
-	    
-	          // Add a war with given context path
-	          // Can add multiple wars this way with different context paths
-	         // tomcat.addWebapp("SampleWebApp.war", "target/SampleWebApp.war");
-	    
-	          return super.getTomcatWebServer(tomcat);
-	      }
-	    
-	    };
-	}
+	
 }
