e0ddffff6af5d7a1f283f52a44808d3609012554
shayan009 <shayan.deba0930@gmail.com>
2021-09-19 20:48:02 +0530

    Removed neflix eureka client dependency and its configurations.

--- src/main/java/com/shayan/config_server/ConfigServerApp.java
+++ src/main/java/com/shayan/config_server/ConfigServerApp.java
@@ -9,14 +9,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigServerApp  {
 
 	
